591ae3f67c8cadd35fbd2655b004145acd0f08f1
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-11-16 16:04:38 +0700

    setting nxtjs config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.myanimelist.net",
-        port: "",
-        pathname: "/images/anime/**",
-      },
-    ],
+    domains: ["cdn.myanimelist.net"],
   },
 };
 
